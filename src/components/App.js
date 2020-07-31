3c3a5981b3a51301ce6e47be1f81902231c7b6c3
rahulmhjn <rahul.mahajan676@gmail.com>
2020-08-01 02:50:19 +0530

    using connect in component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import SongList from "./SongList";
-import { selectSong } from "../actions";
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
-import { act } from "react-dom/test-utils";
-
 import { combineReducers } from "redux";
 
 const songsReducer = () => {
   return [
-    { ttle: "No Scrubs", duration: "4:05" },
+    { title: "No Scrubs", duration: "4:05" },
     { title: "All Star", duration: "2:30" },
     { title: "Macarena", duration: "3:15" },
     { title: "I want it that way", duration: "1:45" },
